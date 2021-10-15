056554aa56766c682df5441059c7baf2fad42a41
Lenainweb <Lenainweb@gmail.com>
2021-10-15 14:54:28 +0300

    create simple bot

--- bot.py
+++ bot.py
@@ -1,81 +1,76 @@
-import random
-import nltk
-import json
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, StackingClassifier
-from sklearn.model_selection import train_test_split
-
-def clean(text):
-  cleaned_text = ''
-  for ch in text.lower():
-    if ch in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя ':
-      cleaned_text = cleaned_text + ch
-  return cleaned_text
-
-def get_intent(text):
-  for intent in BOT_CONFIG['intents'].keys():
-    for example in BOT_CONFIG['intents'][intent]['examples']:
-      cleaned_example = clean(example)
-      cleaned_text = clean(text)
-      if nltk.edit_distance(cleaned_example, cleaned_text) / max(len(cleaned_example), len(cleaned_text)) < 0.4:
-        return intent
-  return 'not_found'
-
-def bot(text):
-  intent = get_intent(text)
-  if intent != 'not_found':
-    return random.choice(BOT_CONFIG['intents'][intent]['responses'])
-  else:
-    return random.choice(BOT_CONFIG['not_found']['responses'])
-
-input_text = ''
-while input_text != 'exit':
-  input_text = input()
-  response = bot(input_text)
-  print(response)
-
-with open('/content/BOT_CONFIG_new.json') as f:
-  BOT_CONFIG = json.load(f)
+from textwithbot import bot
+from setting import TOKEN
 
 
-X = []
-y = []
-for intent in BOT_CONFIG['intents']:
-  for example in BOT_CONFIG['intents'][intent]['examples']:
-    X.append(example)
-    y.append(intent)
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+import logging
 
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-vectorizer = TfidfVectorizer(preprocessor=clean, ngram_range=(1, 3), analyzer='char')
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
-X_train_vectorized = vectorizer.fit_transform(X_train)
-X_test_vectorized = vectorizer.transform(X_test)
-len(vectorizer.get_feature_names())
-
-clf = RandomForestClassifier(n_estimators=300)
-clf.fit(X_train_vectorized, y_train)
-
-clf.score(X_train_vectorized, y_train)
-
-clf.score(X_test_vectorized, y_test)
-
-clf.predict(vectorizer.transform(['где ты живешь?']))
+logger = logging.getLogger(__name__)
 
 
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
 
-def get_intent_by_model(text):
-  return clf.predict(vectorizer.transform([text]))[0]
 
-def bot(text):
-  intent = get_intent_by_model(text)
-  return random.choice(BOT_CONFIG['intents'][intent]['responses'])
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
 
-input_text = ''
-while input_text != 'exit':
-  input_text = input()
-  response = bot(input_text)
-  print(response)
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    out_text = bot(update.message.text)
+    update.message.reply_text(out_text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(TOKEN)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ textwithbot.py
@@ -0,0 +1,88 @@
+import random
+import nltk
+import json
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, StackingClassifier
+from sklearn.model_selection import train_test_split
+
+def clean(text):
+  cleaned_text = ''
+  for ch in text.lower():
+    if ch in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя ':
+      cleaned_text = cleaned_text + ch
+  return cleaned_text
+
+def get_intent(text):
+  for intent in BOT_CONFIG['intents'].keys():
+    for example in BOT_CONFIG['intents'][intent]['examples']:
+      cleaned_example = clean(example)
+      cleaned_text = clean(text)
+      if nltk.edit_distance(cleaned_example, cleaned_text) / max(len(cleaned_example), len(cleaned_text)) < 0.4:
+        return intent
+  return 'not_found'
+
+def bot(text):
+  intent = get_intent(text)
+  if intent != 'not_found':
+    return random.choice(BOT_CONFIG['intents'][intent]['responses'])
+  else:
+    return random.choice(BOT_CONFIG['not_found']['responses'])
+
+# input_text = ''
+# while input_text != 'exit':
+#   input_text = input()
+#   response = bot(input_text)
+#   print(response)
+
+with open('content/BOT_CONFIG_new.json') as f:
+  BOT_CONFIG = json.load(f)
+
+
+X = []
+y = []
+for intent in BOT_CONFIG['intents']:
+  for example in BOT_CONFIG['intents'][intent]['examples']:
+    X.append(example)
+    y.append(intent)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Классификаторы:
+# https://scikit-learn.org/stable/search.html?q=classifier
+# Векторайзеры:
+# https://scikit-learn.org/stable/search.html?q=vectorizer
+
+vectorizer = TfidfVectorizer(preprocessor=clean, ngram_range=(1, 3), analyzer='char')
+
+X_train_vectorized = vectorizer.fit_transform(X_train)
+X_test_vectorized = vectorizer.transform(X_test)
+len(vectorizer.get_feature_names_out())
+
+clf = RandomForestClassifier(n_estimators=300)
+clf.fit(X_train_vectorized, y_train)
+
+clf.score(X_train_vectorized, y_train)
+
+clf.score(X_test_vectorized, y_test)
+
+clf.predict(vectorizer.transform(['где ты живешь?']))
+
+
+
+def get_intent_by_model(text):
+  return clf.predict(vectorizer.transform([text]))[0]
+
+def bot(text):
+  intent = get_intent_by_model(text)
+  return random.choice(BOT_CONFIG['intents'][intent]['responses'])
+
+def main():
+  input_text = ''
+  while input_text != 'exit':
+    input_text = input()
+    response = bot(input_text)
+    print(response)
+
+if __name__ == '__main__':
+    main()
